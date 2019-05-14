fe716d9296c0ae43810051771446278240881120
Victor Velepucha <vvelepuc@users.noreply.github.com>
2019-05-14 13:00:39 -0500

    Se añadio método calcular

---
+++ SlnCalculatorTutorial/CalculatorTutorial/Calculator.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "Calculator.h"
+
+/*
+Calculator::Calculator()
+{
+}
+
+
+Calculator::~Calculator()
+{
+}
+*/
+double Calculator::Calculate(double x, char oper, double y)
+{
+	return 0.0;
+}
---
+++ SlnCalculatorTutorial/CalculatorTutorial/Calculator.h
@@ -0,0 +1,9 @@
+#pragma once
+class Calculator
+{
+public:
+	//Calculator();//Constructor
+	//~Calculator();//destructor
+	double Calculate(double x, char oper, double y);
+};
+
